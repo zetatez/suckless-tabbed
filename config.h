f16b33cbd1d5e1504a5cce215945682e157484ee
Dionysus <zetatez@icloud.com>
2023-02-18 03:24:27 +0800

    cmt: merge: tabbed-bar-height-0.6.diff

--- config.h
+++ config.h
@@ -12,6 +12,7 @@ static const char before[]      = "<";
 static const char after[]       = ">";
 static const char titletrim[]   = "...";
 static const int  tabwidth      = 200;
+static const int  barHeight	    = 20; // patch: tabbed-bar-height
 static const Bool foreground    = True;
 static       Bool urgentswitch  = False;
 
--- tabbed.c
+++ tabbed.c
@@ -984,7 +984,8 @@ setup(void)
 	screen = DefaultScreen(dpy);
 	root = RootWindow(dpy, screen);
 	initfont(font);
-	bh = dc.h = dc.font.height + 2;
+//bh = dc.h = dc.font.height + 2; // patch: tabbed-bar-height
+ 	bh = dc.h = barHeight;          // patch: tabbed-bar-height
 
 	/* init atoms */
 	wmatom[WMDelete] = XInternAtom(dpy, "WM_DELETE_WINDOW", False);
